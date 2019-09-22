6d236040436a9bb09ec23ab72fb955c21dbaf5ab
Anderson <andersons.code@gmail.com>
2019-09-22 18:37:44 -0300

    feat: mocking dashboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.scss';
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
 // Containers
-const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
+const DashboardLayout = React.lazy(() => import('./containers/DashboardLayout'));
 
 // Pages
 const Login = React.lazy(() => import('./views/Pages/Login'));
@@ -25,7 +25,7 @@ class App extends Component {
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+              <Route path="/" name="Home" render={props => <DashboardLayout {...props}/>} />
             </Switch>
           </React.Suspense>
       </HashRouter>
